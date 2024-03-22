de7beb9bcb8cd1e02cbf99f89977df5bc0dfe548
Chris Lawrence <chrismlawrence89@gmail.com>
2024-03-22 17:49:11 +1100

    nvim tweaks

--- config/nvim/lua/plugins/nvim-test.lua
+++ config/nvim/lua/plugins/nvim-test.lua
@@ -5,6 +5,7 @@ return {
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/nvim-nio",
     "olimorris/neotest-rspec",
   },
   config = function()
